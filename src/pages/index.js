6e91e717cb62924d6af37729613dd49a9832d823
Laxmikant Kendre <kendrelaxman@users.noreply.github.com>
2019-09-28 20:16:36 -0500

    twitter integrate

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,19 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
+      <div>
+        <a
+          class="twitter-timeline"
+          href="https://twitter.com/imkendre?ref_src=twsrc%5Etfw"
+        >
+          Tweets by imkendre
+        </a>{" "}
+        <script
+          async
+          src="https://platform.twitter.com/widgets.js"
+          charset="utf-8"
+        />
+      </div>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
